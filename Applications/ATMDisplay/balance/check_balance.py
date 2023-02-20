b62c46df604fbdae474ac97c563bec0b780629ac
The TechSnippets <83929052+TheTechSnippets@users.noreply.github.com>
2023-02-20 23:49:29 +0530

    Delete Applications/ATMDisplay directory

--- Applications/ATMDisplay/balance/check_balance.py
+++
@@ -1,5 +0,0 @@
-
-def check_balance():
-    print("checking the balance....")
-    # do some ops
-    return 1000;
--- Applications/ATMDisplay/balance/withdraw_balance.py
+++
@@ -1,5 +0,0 @@
-
-def withdraw_balance(amount):
-    print(f"Withdrawing {amount} from you account..")
-    # do some ops
-    return "SUCCESS"
